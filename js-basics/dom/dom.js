766a4ed5166550e046e41614684984cf6a2d073e
funnyling <funnyling444@gmail.com>
2017-01-31 00:28:12 +0300

    dom

--- js-basics/dom/dom.js
+++ js-basics/dom/dom.js
@@ -3,5 +3,10 @@
  * @version 29.01.2017
  */
 (function () {
+    var myNavigator = navigator.userAgent;
+    var curLocation = location.href;
+    location.href = 'http://www.google.com';
 
+    myNavigator
+    curLocation
 })();
