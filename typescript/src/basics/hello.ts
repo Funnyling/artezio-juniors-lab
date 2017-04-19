6a719eb61e40e701716149f9ebfc288ba394116a
funnyling <funnyling444@gmail.com>
2017-04-19 17:42:32 +0300

    typescript: basic types

--- typescript/src/basics/hello.ts
+++ typescript/src/basics/hello.ts
@@ -1,10 +1,11 @@
 /**
  * Created by ntishkevich on 19.04.2017.
  */
-function greeter(person) {
+function greeter(person: string) {
+    console.log(`${person}`);
     return "Hello, " + person;
 }
 
-var user = "Jane User";
+var user = 'Steve';
 
 document.body.innerHTML = greeter(user);
