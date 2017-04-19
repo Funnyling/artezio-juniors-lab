69431d2d199c409761962915d32bb7584c87956a
funnyling <funnyling444@gmail.com>
2017-04-19 15:16:24 +0300

    typescript: basic types

--- typescript/src/basics/basic_types.ts
+++ typescript/src/basics/basic_types.ts
@@ -18,13 +18,85 @@ let fruits: Array<string> = ['Mango', 'Pear', 'Raspberry', 'Watermelon'];
 
 let tuple: [string, number];
 tuple = ['Hello', 42];
-// tuple = [42, 'Hello'];
+tuple = [42, 'Hello'];
 
 tuple = ['Hello', 42, 100, 0, 'name'];
 
 tuple[0].substr(1);
-// tuple[1].substr(1);
-tuple[6];
+tuple[1].substr(1); // error substr does not exist on type number
+
+tuple[3] = 'world'; // OK, 'string' может быть присвоен 'string | number‘
+tuple[5].toString(); // OK, 'string' и 'number' оба имеют 'toString‘
+tuple[6] = true; // Ошибка, 'boolean' не 'string | number'
 
 let foo: undefined = undefined;
 let bar: null = null;
+
+enum Color {Red = 1, Green, Blue}
+let colorName: string = Color[2];
+
+let notSure: any = 4;
+notSure = "maybe a string instead";
+notSure = false; // ok, это определенно boolean
+
+
+let list: any[] = [1, true, "free"];
+
+list[1] = 100;
+
+let notSure: any = 4;
+notSure.ifItExists(); // ifItExists может существовать на этапе исполнения
+notSure.toFixed(); // метод toFixed существует (но компилятор не проверяет это)
+
+let prettySure: Object = 4;
+prettySure.toFixed(); //  Ошибка: Свойство 'toFixed' не существует у типа 'Object'.
+
+
+function error(message: string): never {
+    throw new Error(message);
+}
+
+// Предполагаемый тип возврата - never
+function fail() {
+    return error("Something failed");
+}
+
+// Функция возвращающая never имеет недостижимую точку возврата
+function infiniteLoop(): never {
+    while (true) {
+    }
+}
+
+function greeter(person: string = 'Bill', msg: string = 'Hello'): void {
+    console.log(`${msg},  ${person}`);
+}
+greeter('Steve');
+greeter();
+
+function greeter(person: string, msg?: string): void {
+    console.log(`${msg = 'Hello'},  ${person}`);
+}
+
+greeter('Steve');
+
+greeter('Steve', undefined);
+
+function greeter(person: string, msg: string = 'Hello'): void {
+    console.log(`${msg},  ${person}`);
+}
+
+let unusable: void = undefined;
+
+// Нельзя присвоить ничего другого этим переменным!
+let u: undefined = undefined;
+let n: null = null;
+
+let nullableString: string | null | undefined = null;
+
+let neverVar: never = 'my string';
+
+let neverVar: never;
+let newString: string = neverVar; // error substr does not exist on type number
+
+let someValue: any = 'this is a string';
+let strLength: number = (someValue as string).length;
